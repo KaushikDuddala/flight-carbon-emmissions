b33ad47162af18eae2327c59c7d3c68ac5e97611
KaushikDuddala <kaushik.duddala@gmail.com>
2024-07-03 19:09:34 -0500

    backend done

--- src/index.js
+++ src/index.js
@@ -8,8 +8,43 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const token = require("../config.json").carbonEmmissionsToken
+
+const url2 = 'https://www.carboninterface.com/api/v1/estimates';
+
+const options = {
+	'Authorization': `Bearer ${token}`,
+	'Content-Type': 'application/json',
+};
+
+
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const url = request.url
+
+		
+		if (url.includes('emmissions')) {
+			const { searchParams } = new URL(request.url)
+			const body = {
+				type: "flight",
+				passengers: searchParams.get("numOfPassengers"),
+				legs: [
+					{ departure_airport: searchParams.get("departure"), destination_airport: searchParams.get("destination"), cabin_class: searchParams.get("cabinClass")}
+				]
+			};
+
+
+			const data = await fetch(url2, {
+				method:"POST",
+				headers: options,
+				body: JSON.stringify(body)
+			}).then(response => { return response.json() })
+
+			return new Response(JSON.stringify(data), {
+				headers: { 'content-type': 'application/json' },
+			});
+		}
+		
 	},
 };
